2d641e378c9959c241f7bc50193a1c220dbb20c9
FrankQi <24457117+863zq@users.noreply.github.com>
2025-05-02 11:44:30 +0800

    Update Delocalized.py

--- Code/Delocalized.py
+++ Code/Delocalized.py
@@ -6,19 +6,16 @@ import pytorch_lightning as pl
 from torchvision import transforms
 from torchmetrics.functional import pearson_corrcoef, structural_similarity_index_measure, peak_signal_noise_ratio
 
-def autocorrelation(spk): 
+def autocorrelation(spk): # Derive speckle autocorrelation
     # Ref https://github.com/qhzhang95/PEACE_Speckle/blob/main/1_read%20the%20raw%20image%20and%20calculate%20the%20autocorrelations.ipynb
-    spk = (spk-spk.min())/(spk.max()-spk.min()) # normalization
+    spk = (spk-spk.min())/(spk.max()-spk.min()) # Normalization
     at = torch.fft.fft2(spk).abs().pow(2)
     at = torch.fft.ifftshift(torch.fft.ifft2(at))/spk.shape[0]/spk.shape[1]
     at = (torch.abs(at)-torch.mean(spk)**2)/(torch.mean(spk**2)-(torch.mean(spk))**2)
     at = at - (torch.mean(at)).min() # subtract background
     return at
-def normalization(x): # normalize to [0,1]
-    Min = x.min(-2,keepdims=True)[0].min(-1,keepdims=True)[0]
-    Max = x.max(-2,keepdims=True)[0].max(-1,keepdims=True)[0]
-    return (x-Min)/(Max-Min)
-def entropy(x,bit=16): # derive entropy of input
+    
+def entropy(x,bit=16): # Derive entropy of input
     x = torch.ceil(x*2**bit)
     _,count = torch.unique(x,return_counts=True)
     count   = count/count.sum()
@@ -26,9 +23,9 @@ def entropy(x,bit=16): # derive entropy of input
     return result
 
 class Model(pl.LightningModule):
-    def __init__(self, in_dim, out_dim):
+    def __init__(self, lr, in_dim, out_dim):
         super().__init__()
-        self.lr, self.in_dim, self.out_dim, self.val_step_outputs, self.training_step_outputs = opt.lr, in_dim, out_dim, [], []
+        self.lr, self.in_dim, self.out_dim = lr, in_dim, out_dim
         self.model = torch.nn.Linear(in_dim*in_dim, out_dim*out_dim).to(torch.complex64)
     def forward(self,x):
         x = x.reshape(-1, self.in_dim*self.in_dim).to(torch.complex64)
